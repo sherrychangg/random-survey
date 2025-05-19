d2d6e7a38c7c0bd731f0907e85705bc1e7625c42
sherrychangg <ssherry1217@gmail.com>
2025-05-19 19:45:29 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,6 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232',
+	'https://www.surveycake.com/s/AO4Xm',
+	'https://www.surveycake.com/s/KXArl',
 ];
