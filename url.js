935da7ae51b4bc3c159fb93010743cc8cdfb4d66
sherrychangg <ssherry1217@gmail.com>
2025-06-18 03:50:11 +0800

    Update url.js

--- url.js
+++ url.js
@@ -10,7 +10,6 @@
 const urls = [
 	'https://www.surveycake.com/s/gGMMr',
 	'https://www.surveycake.com/s/GQBBr', 
-	'https://www.surveycake.com/s/PZQQM',
 	'https://www.surveycake.com/s/zKdO2',
 	'https://www.surveycake.com/s/ZGX32',
 	'https://www.surveycake.com/s/lGM3L'
